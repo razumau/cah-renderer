5e5f8e16bcc8f0a43478489dc05cbd5db972848d
Jury Razumau <mail@razumau.net>
2023-09-16 18:35:13 +0200

    number batches with a leading 0

--- render.rb
+++ render.rb
@@ -6,8 +6,9 @@ def render_prompts(prompts)
   template = ERB.new(File.read("templates/prompt.html.erb"))
 
   prompts.each_slice(20).each_with_index do |batch_of_prompts, index|
-    rendered = template.result_with_hash({ prompts: batch_of_prompts})
-    File.write("rendered/prompts_#{index + 1}.html", rendered)
+    rendered = template.result_with_hash({ prompts: batch_of_prompts })
+    batch_number = format('%02d', index + 1)
+    File.write("rendered/prompts_#{batch_number}.html", rendered)
   end
 end
 
