629e17d5bd7e9ac7a9ebd8940108eb2d6e564b05
Jury Razumau <mail@razumau.net>
2023-09-16 18:22:43 +0200

    extract text from spreadsheet cells

--- fetch_spreadsheet.rb
+++ fetch_spreadsheet.rb
@@ -9,15 +9,15 @@ SPREADSHEET_KEY = ENV["SPREADSHEET_KEY"]
 GOOGLE_API_KEY = ENV["GOOGLE_API_KEY"]
 
 def fetch_prompts
-  fetch_data("prompts", "D").values.map { |text| Card[:prompt, text] }
+  fetch_data("prompts", "D").map { |text| Card[:prompt, text] }
 end
 
 def fetch_responses
-  fetch_data("responses", "D").values.map { |text| Card[:response, text] }
+  fetch_data("responses", "D").map { |text| Card[:response, text] }
 end
 
 def fetch_data(sheet_name, column)
   sheets = Google::Apis::SheetsV4::SheetsService.new
   sheets.key = GOOGLE_API_KEY
-  sheets.get_spreadsheet_values(SPREADSHEET_KEY, "#{sheet_name}!#{column}2:#{column}")
+  sheets.get_spreadsheet_values(SPREADSHEET_KEY, "#{sheet_name}!#{column}2:#{column}").values.map(&:first)
 end
