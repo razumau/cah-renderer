d679dc023e41552e6172306d18523a0b53524b94
Jury Razumau <mail@razumau.net>
2023-09-16 17:39:59 +0200

    render 55 test prompts

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "card"
 require_relative "fetch_spreadsheet"
+require_relative "render"
 
-prompts = fetch_prompts
-responses = fetch_responses
-puts "Fetched #{prompts.size} prompts and #{responses.size} responses"
+# prompts = fetch_prompts
+# responses = fetch_responses
+# puts "Fetched #{prompts.size} prompts and #{responses.size} responses"
+prompts = (1..55).map { |index| Card[:prompt, "Text for a prompt ##{index}: ___________"] }
+
+render_prompts(prompts)
