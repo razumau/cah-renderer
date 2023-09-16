218c3580a019e220e6b7f7e7e510587dbe70b0a5
Jury Razumau <mail@razumau.net>
2023-09-16 17:38:50 +0200

    print all html in the `rendered` folder

--- print_to_pdf.js
+++ print_to_pdf.js
@@ -4,8 +4,9 @@ const path = require('path');
 
 (async () => {
     const browser = await puppeteer.launch({ headless: "new" });
+    const rendered_folder = "rendered";
 
-    const directoryPath = path.join(__dirname, 'rendered');
+    const directoryPath = path.join(__dirname, rendered_folder);
     fs.readdir(directoryPath, async (err, files) => {
         if (err) {
             console.error('Error reading directory ', err);
@@ -21,9 +22,12 @@ const path = require('path');
             console.log('Printing', absolutePath);
 
             await page.goto(`file://${absolutePath}`, { waitUntil: 'networkidle2' });
+
             const margins = { bottom: 20, top: 20, left: 20, right: 20 }
-            await page.pdf({ path: 'rendered/prompt.pdf', format: 'A4', scale: 0.8, margin: margins });
-            console.log(`Saved ${file}.pdf`);
+            const pdf_path = path.join(rendered_folder, file.replace("html", "pdf"))
+            await page.pdf({ path: pdf_path, format: 'A4', scale: 0.8, margin: margins });
+            console.log(`Saved ${pdf_path}`);
+
             await page.close();
         }
 
