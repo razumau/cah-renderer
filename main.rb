d4806b370b35c4cb792215a54a8742961ed28d7e
Jury Razumau <mail@razumau.net>
2023-09-16 18:35:29 +0200

    render prompts and responses in a single style

--- main.rb
+++ main.rb
@@ -4,9 +4,8 @@ require_relative "card"
 require_relative "fetch_spreadsheet"
 require_relative "render"
 
-# prompts = fetch_prompts
-# responses = fetch_responses
-# puts "Fetched #{prompts.size} prompts and #{responses.size} responses"
-prompts = (1..55).map { |index| Card[:prompt, "Text for a prompt ##{index}: ___________"] }
+prompts = fetch_prompts
+responses = fetch_responses
+puts "Fetched #{prompts.size} prompts and #{responses.size} responses"
 
-render_prompts(prompts)
+render_prompts(prompts + responses)
