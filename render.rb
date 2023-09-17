76a9ebf570e9ef6276ca4ca026575c1295625b60
Jury Razumau <mail@razumau.net>
2023-09-17 18:13:15 +0200

    switch font to Inter

--- render.rb
+++ render.rb
@@ -6,6 +6,7 @@ def render_prompts(prompts)
   template = ERB.new(File.read("templates/prompt.html.erb"))
 
   prompts.each_slice(20).each_with_index do |batch_of_prompts, index|
+    puts "Rendering batch ##{index + 1} to HTML"
     rendered = template.result_with_hash({ prompts: batch_of_prompts })
     batch_number = format('%02d', index + 1)
     File.write("rendered/prompts_#{batch_number}.html", rendered)
