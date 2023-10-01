52125f64ee446e81546156b2e709b7ea52352243
Jury Razumau <mail@razumau.net>
2023-10-01 20:19:10 +0200

    format prompts before rendering
    Replaces \ with a <br> (to enable proper multi-line prompts, e.g., verses).
    Add a period at the end of a prompt.
    Makes blanks the same size.

--- main.rb
+++ main.rb
@@ -3,7 +3,16 @@
 require_relative "fetch_spreadsheet"
 require_relative "render"
 
-prompts = fetch_prompts
+def format_prompt(prompt)
+  prompt.gsub!('\\', '<br>')
+  prompt.gsub!(/_+/, '__________')
+  prompt.chomp!(' ')
+  prompt << '.' unless %w[. ! ?].include?(prompt.chars.last)
+
+  prompt
+end
+
+prompts = fetch_prompts.map { |prompt| format_prompt(prompt) }
 responses = fetch_responses
 puts "Fetched #{prompts.size} prompts and #{responses.size} responses"
 
